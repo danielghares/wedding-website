732dc08af98210c85c50f0bd26ee63d8a8ff40b4
danielghares <69513588+danielghares@users.noreply.github.com>
2022-11-26 11:56:24 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyvbf-2ORwfUjwRZkFTdfylSKzXp6EYLGtqquP9OGRT3H6uzb_1EUk7ljGzTieHXAeGdA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbxXGMmFC-mMQxrYH9ujwgKIdTqSsdwbsRLGVOsI_BGadsvXKMWPrhvhYOuRPfhDsaiS/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
